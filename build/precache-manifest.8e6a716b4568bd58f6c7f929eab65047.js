f260510a3933b3f78bb2721759982c80c38e35fc
Zanzer Tem <dan.needham@gmail.com>
2020-08-28 06:36:54 +0100

    Added portrait

---
+++ build/precache-manifest.8e6a716b4568bd58f6c7f929eab65047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6559a92d3cfca85f905527a865f82e89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7689c73e7c2f17708d7",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "442f4a7d0659b6734948",
+    "url": "/static/js/2.0cb8b18f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0cb8b18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7689c73e7c2f17708d7",
+    "url": "/static/js/main.b3d7a612.chunk.js"
+  },
+  {
+    "revision": "8c5eaf12d3ec9a531946",
+    "url": "/static/js/runtime-main.36232810.js"
+  }
+]);
