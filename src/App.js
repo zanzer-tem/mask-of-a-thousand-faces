8e9e059de36cbbbe1ea184f668041482abe20a0b
Zanzer Tem <dan.needham@gmail.com>
2020-08-28 06:40:23 +0100

    Added portrait

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header >
+          <h1>Zanzer's Dungeon</h1>
+          <h2><a href="https://zanzer-tem.github.io/zanzers-dungeon/" title="Link to Zanzer's Dungeon; Dungeon Master tools">Dungeon Master Tools</a></h2>
+      </header>
+      <main className="main">
+          <h3>Mask of a Thousand Faces</h3>
+          <p>NPC avatar generator.</p>
+          <div class="mask-of-a-thousand-faces">
+
+          </div>
+          <p>Randomize</p>
+      </main>
+      <footer>
+          Copyright © 2020 <a href="https://zanzer-tem.github.io/zanzers-dungeon/" title="Link to Zanzer's Dungeon; Dungeon Master tools">Zanzer's Dungeon</a>. All rights reserved. 
+      </footer>
+  </div>
+  );
+}
+
+export default App;
