b9f23e0907139aa54c6ff332561bc9c12eadc58b
Zanzer Tem <dan.needham@gmail.com>
2020-11-23 22:24:20 +0000

    Updates

---
+++ precache-manifest.daa7d69cc632a96c7a460c0edd2b9f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c8a0331560e0856d692e2f9f5bb724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c10d6ee06c793378c774",
+    "url": "/static/css/2.c8b4cdf6.chunk.css"
+  },
+  {
+    "revision": "78b5a392f0314ac8f4a1",
+    "url": "/static/css/main.b2862c29.chunk.css"
+  },
+  {
+    "revision": "c10d6ee06c793378c774",
+    "url": "/static/js/2.1c80120a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1c80120a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b5a392f0314ac8f4a1",
+    "url": "/static/js/main.318f5089.chunk.js"
+  },
+  {
+    "revision": "8c5eaf12d3ec9a531946",
+    "url": "/static/js/runtime-main.36232810.js"
+  }
+]);
