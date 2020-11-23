e4ae6b0d3d0f997e649b8da79fb81997bf351faa
Zanzer Tem <dan.needham@gmail.com>
2020-11-23 22:19:26 +0000

    Updates

---
+++ precache-manifest.ff1d044ecaf3e2bb072421d7c10349f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ffd9a743807eba44fb8b960815ee65c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "692948187d3c29012913",
+    "url": "/static/css/2.c8b4cdf6.chunk.css"
+  },
+  {
+    "revision": "7b458ace1f38270f7925",
+    "url": "/static/css/main.b2862c29.chunk.css"
+  },
+  {
+    "revision": "692948187d3c29012913",
+    "url": "/static/js/2.a5359f58.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.a5359f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b458ace1f38270f7925",
+    "url": "/static/js/main.e4e5d7d2.chunk.js"
+  },
+  {
+    "revision": "7a303c85ac35b25b3fbb",
+    "url": "/static/js/runtime-main.77d33e86.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mask-of-a-thousand-faces/precache-manifest.ee7d7f1107c7ded288fa0d21c23f47dc.js"
+  "/precache-manifest.ff1d044ecaf3e2bb072421d7c10349f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mask-of-a-thousand-faces/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
